356e3e2907deeea3114de0fe38a8be0fb44ceca8
nguyen.dk <nd2004lk13@gmail.com>
2025-12-05 20:46:43 +0700

    fix: update avatar image import to use default avatar from new assets directory

--- src/components/Comment/CommentDetail.tsx
+++ src/components/Comment/CommentDetail.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import avatar from '../../assets/images/avatar.png';
+import avatar from '../../assets/images/default_avatar.jpg';
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/vi";

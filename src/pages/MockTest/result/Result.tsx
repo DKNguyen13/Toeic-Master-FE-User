362bf7018c1d83e7c32063cbf8e3370fed3dc887
NhatNguyen1784 <nguyennhatnguyen1782004@gmail.com>
2025-12-17 20:48:19 +0700

    feat: add user answers handling in Result component and pass to ResultPage

--- src/pages/MockTest/result/Result.tsx
+++ src/pages/MockTest/result/Result.tsx
@@ -23,6 +23,23 @@ ChartJS.register(
   LinearScale
 );
 
+type answers = {
+  questionId: string;
+  question: string;
+  part: number;
+  questionNumber: number;
+  choices : {
+    label: string;
+    text: string;
+  }
+  userAnswer: {
+    isFlagged: boolean;
+    isSkipped: boolean;
+    selectedAnswer: string | null;
+  };
+  correctAnswer: string;
+};
+
 type ResultProps = {
   id: string;
   testTitle: string;
@@ -34,6 +51,7 @@ type ResultProps = {
   readingScore: number;
   totalScore: number;
   isFullTest: boolean;
+  answers: answers[];
 };
 
 const Result: React.FC<ResultProps> = ({
@@ -47,25 +65,42 @@ const Result: React.FC<ResultProps> = ({
   readingScore,
   totalScore,
   isFullTest,
+  answers
 }) => {
+  console.log("user answers:", answers);
   const navigate = useNavigate();
   const handleGoBack = () => {
     navigate(`/history`);
   };
   
   const fetchFeedback = async () => {
+    console.log("user answers:", answers);
     if (feedback) return;
 
     setLoadingFb(true);
 
+    const formattedAnswers = answers.map(a => ({
+      questionId: a.questionId,
+      part: a.part,
+      questionNumber: a.questionNumber,
+      correctAnswer: a.correctAnswer,
+      selectedAnswer: a.userAnswer.selectedAnswer,
+      isSkipped: a.userAnswer.isSkipped,
+      isFlagged: a.userAnswer.isFlagged,
+    }));
+
     try {
       const res = await api.post("/analysis/result", {
-        correctAnswers,
-        wrongAnswers,
-        skippedQuestions,
-        totalQuestions,
-        listeningScore,
-        readingScore,
+        summary: {
+          correctAnswers,
+          wrongAnswers,
+          skippedQuestions,
+          totalQuestions,
+          listeningScore,
+          readingScore,
+        },
+        isFullTest,
+        answers: formattedAnswers
       });
       setFeedback(res.data.feedback);
     } catch (err) {

b9ad3d7078982adabbdda081d9017c415f036f5a
nguyen.dk <nd2004lk13@gmail.com>
2025-12-18 11:53:38 +0700

    fix: update API base URL to production endpoint

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://toeic-master-be.onrender.com/api",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });

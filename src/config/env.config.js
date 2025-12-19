c9483292f8ff997a56834d36f02423550d5a4c58
nguyen.dk <nd2004lk13@gmail.com>
2025-12-19 08:57:01 +0700

    fix: fix bug env config

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,5 +1,5 @@
 export const config = {
     // Website URLs
-    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
-    googleClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID || '',
+    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
+    googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID || '',
 };
